f52fbd652d6300e55ee344a8d774ab8986512058
cfaucher18 <cfaucher@sjnma.org>
2018-12-11 08:30:14 -0600

    changes

--- f2/js/index.js
+++ f2/js/index.js
@@ -5,5 +5,8 @@ document.getElementById("annoy").innerHTML = "I am annoyed";
 while(true){
 alert(fname + " are we there yet");
 }
-
+function mark_present()
+{
+    var cadet_name = $('#cadet_name').val();
+    alert(cadet_name=" is here!");
 }
